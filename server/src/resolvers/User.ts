579eeecb20e0c7907b96e078aa92292047689550
Vasil Kosturski <vasilkosturski@gmail.com>
2022-05-04 09:19:48 +0300

    Refactored User.ts

--- server/src/resolvers/User.ts
+++ server/src/resolvers/User.ts
@@ -8,29 +8,19 @@ export const User = {
   posts: (parent: UserParentType, __: any, { userInfo, prisma }: Context) => {
     const isOwnProfile = parent.id === userInfo?.userId;
 
-    if (isOwnProfile) {
-      return prisma.post.findMany({
-        where: {
-          authorId: parent.id,
-        },
-        orderBy: [
-          {
-            createdAt: "desc",
-          },
-        ],
-      });
-    } else {
-      return prisma.post.findMany({
-        where: {
-          authorId: parent.id,
-          published: true,
-        },
-        orderBy: [
-          {
-            createdAt: "desc",
-          },
-        ],
-      });
+    var filter:any = {}
+    filter.authorId = parent.id;
+    if (isOwnProfile){
+      filter.published = true;
     }
+
+    return prisma.post.findMany({
+      where: filter,
+      orderBy: [
+        {
+          createdAt: "desc",
+        },
+      ],
+    });
   },
 };
